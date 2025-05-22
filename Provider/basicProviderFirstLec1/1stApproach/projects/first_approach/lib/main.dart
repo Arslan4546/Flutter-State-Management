643dce4d4d8ee1332990753f6a0fc8c14b151727
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-22 22:17:13 +0500

    udpate

--- Provider/basicProviderFirstLec1/1stApproach/projects/first_approach/lib/main.dart
+++ Provider/basicProviderFirstLec1/1stApproach/projects/first_approach/lib/main.dart
@@ -17,17 +17,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Provider<Student>(
-        create: (context) {
-          return Student();
-        },
-        child: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
 
-      // home: ChangeNotifierProvider(
-      //   create: (context) => Counter(),
+      // home: Provider<Student>(
+      //   create: (context) {
+      //     return Student();
+      //   },
       //   child: const MyHomePage(title: 'Flutter Demo Home Page'),
       // ),
+      home: ChangeNotifierProvider(
+        create: (context) => Counter(),
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
@@ -48,8 +48,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var std = Provider.of<Student>(context);
-    print("Roll No: ${std.rollNo}");
+    // var std = Provider.of<Student>(context);
+    // print("Roll No: ${std.rollNo}");
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
