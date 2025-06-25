b180993318cf834ea8129f56728b06bfd19e8d52
Arslan Tariq <arslantariq4546@gmail.com>
2025-06-24 19:53:45 -0700

    futureProvider completed

--- riverPod/FutureRiverpod/code/future_riverpod_provider_practice/lib/main.dart
+++ riverPod/FutureRiverpod/code/future_riverpod_provider_practice/lib/main.dart
@@ -24,41 +24,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[ApiPage()],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
 class ApiPage extends ConsumerWidget {
   const ApiPage({super.key});
 
@@ -73,12 +38,6 @@ class ApiPage extends ConsumerWidget {
             data: (data) => Text(data),
             error: (error, stackTrace) => Text(error.toString()),
             loading: () => CircularProgressIndicator(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              ref.read(apiProvider).fetchData();
-            },
-            child: const Text('Refresh Data'),
           ),
         ],
       ),
