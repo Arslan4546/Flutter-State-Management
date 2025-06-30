d71116c5290cab6503d2120e79378edf7bb96809
Arslan Tariq <arslantariq4546@gmail.com>
2025-06-30 11:40:01 -0700

    update

--- riverPod/APIWithRiverPod/code/api_practice/lib/provider.dart
+++ riverPod/APIWithRiverPod/code/api_practice/lib/provider.dart
@@ -1,5 +1,5 @@
 import 'package:api_practice/API_Service/product_api_provider.dart';
-import 'package:api_practice/album_state.dart';
+import 'package:api_practice/product_states.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final productAPIProvider =
