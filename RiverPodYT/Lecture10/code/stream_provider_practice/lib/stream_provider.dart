1147a704c861c82dac993e44642841a091c796c0
Arslan Tariq <arslantariq4546@gmail.com>
2025-06-29 19:40:33 -0700

    steam provider start

---
+++ RiverPodYT/Lecture10/code/stream_provider_practice/lib/stream_provider.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final streamProvider = StreamProvider<double>((ref) async* {
+  final random = Random();
+  double currentPrice = 100.0;
+  // this below the logic for changing the price periodically
+  await Future.delayed(Duration(seconds: 1));
+  currentPrice += random.nextDouble() * 4 - 2;
+  yield double.parse(currentPrice.toStringAsFixed(2));
+});
