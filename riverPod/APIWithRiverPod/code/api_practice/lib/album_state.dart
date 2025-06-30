ef9f1dfd5a765ad7244ad87efa2ef8aa94416a7e
Arslan Tariq <arslantariq4546@gmail.com>
2025-06-30 08:31:34 -0700

    update

--- riverPod/APIWithRiverPod/code/api_practice/lib/album_state.dart
+++ riverPod/APIWithRiverPod/code/api_practice/lib/album_state.dart
@@ -1,5 +1,18 @@
+import 'package:api_practice/API_Service/api_service.dart';
+import 'package:api_practice/API_Service/product_model.dart';
+
 sealed class AlbumState {}
 
 class AlbumInitialState extends AlbumState {}
 
 class AlbumLoadingState extends AlbumState {}
+
+class AlbumLoadedState extends AlbumState {
+  final List<ProductModel> productList;
+  AlbumLoadedState({required this.productList});
+}
+
+class AlbumErrorState extends AlbumState {
+  final String errorMessage;
+  AlbumErrorState({required this.errorMessage});
+}
