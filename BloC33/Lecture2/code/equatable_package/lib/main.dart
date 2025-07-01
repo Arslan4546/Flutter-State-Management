c6a1daac008f426e6097d8d30eb8a97c3b538ac1
Arslan Tariq <arslantariq4546@gmail.com>
2025-06-30 22:57:13 -0700

    update

--- BloC33/Lecture2/code/equatable_package/lib/main.dart
+++ BloC33/Lecture2/code/equatable_package/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,46 @@ class Home extends StatelessWidget {
 
       ]
     ),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Person person1 = Person(age: 23, name: "Arslan");
+          Person person2 = Person(age: 23, name: "Mannan");
+          print(person1 == person2);
+          // now this print true
+          // Person person1 = Person(age: 23, name: "Arslan");
+          // Person person2 = Person(age: 23, name: "Arslan");
+          // print(person1 == person2);
+          // this will print the flase becasue it is comparing the memory location of the objects
+        },
+      ),
     );
   }
 }
+
+class Person extends Equatable {
+  String name;
+  int age;
+  Person({required this.age, required this.name});
+
+  @override
+  List<Object?> get props => [name, age];
+}
+
+// this is the code before the equatable package
+// class Person {
+//   String name;
+//   int age;
+//   Person({required this.age, required this.name});
+
+//   @override
+//   bool operator ==(Object other) {
+//     return identical(this, other) ||
+//         other is Person &&
+//             runtimeType == other.runtimeType &&
+//             name == other.name &&
+//             age == other.age;
+//   }
+
+//   @override
+//   int get hashCode => name.hashCode ^ age.hashCode;
+// }
