a563c58a0cfc698599fe356476a99df60fa720ae
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-02 11:16:48 +0500

    update

---
+++ BloC/vanellaBloC/code/vanella_implmentation_bloc/android/app/src/main/kotlin/com/example/vanella_implmentation_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vanella_implmentation_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ BloC/vanellaBloC/code/vanella_implmentation_bloc/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text('0', style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
