b30c8a8006a5b57371e6f6fdfd296ada0df1aa12
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-03 10:11:26 +0500

    update

--- BloCYT33/Lecture6/code/handle_multiple_states_in_bloc/lib/Bloc/switch_bloc/bloc/switch_bloc.dart
+++ BloCYT33/Lecture6/code/handle_multiple_states_in_bloc/lib/Bloc/switch_bloc/bloc/switch_bloc.dart
@@ -1,12 +1,18 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'switch_event.dart';
 part 'switch_state.dart';
 
 class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
   SwitchBloc() : super(SwitchState()) {
-    on<EnableOrDisableNotification>();
+    on<EnableOrDisableNotification>(_enableOrDisableNotification);
+  }
+
+  void _enableOrDisableNotification(
+    EnableOrDisableNotification event,
+    Emitter<SwitchState> emit,
+  ) {
+    emit(state.copyWith(isSwitch: !state.isSwitch));
   }
 }
