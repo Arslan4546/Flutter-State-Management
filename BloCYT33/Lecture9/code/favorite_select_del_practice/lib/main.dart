4698898b9176a2d31b1b81b7b7a28ff9dcf386f9
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-05 11:22:38 +0500

    update

--- BloCYT33/Lecture9/code/favorite_select_del_practice/lib/main.dart
+++ BloCYT33/Lecture9/code/favorite_select_del_practice/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:favorite_select_del_practice/Bloc/favorite_bloc/bloc/favorite_bloc.dart';
+import 'package:favorite_select_del_practice/Repositories/favorite_repository.dart';
 import 'package:favorite_select_del_practice/UI/favorite_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(brightness: Brightness.dark),
-      home: const FavoriteScreen(),
+    return BlocProvider(
+      create: (context) => FavoriteBloc(FavoriteRepository()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: const FavoriteScreen(),
+      ),
     );
   }
 }
