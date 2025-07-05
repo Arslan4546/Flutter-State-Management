0b2c3deea239c276ad326a06e528cc1478ee0584
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-05 11:42:22 +0500

    update

--- BloCYT33/Lecture9/code/favorite_select_del_practice/lib/UI/favorite_screen.dart
+++ BloCYT33/Lecture9/code/favorite_select_del_practice/lib/UI/favorite_screen.dart
@@ -1,10 +1,32 @@
+import 'package:favorite_select_del_practice/Bloc/favorite_bloc/bloc/favorite_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorites')),
+      body: BlocBuilder<FavoriteBloc, FavoriteState>(
+        builder: (context, state) {
+          switch (state.listStatus) {
+            case ListStatus.loading:
+              return const Center(child: CircularProgressIndicator());
+            case ListStatus.success:
+              return ListView.builder(
+                itemCount: state.favoriteItemsList.length,
+                itemBuilder: (context, index) {
+                  final item = state.favoriteItemsList[index];
+                  return ListTile(title: Text(item.value));
+                },
+              );
+            case ListStatus.failed:
+              return const Center(child: Text('Error loading favorites'));
+          }
+        },
+      ),
+    );
   }
 }
