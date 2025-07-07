7c03d912270c6af9c46539866ac32db9272c3fa0
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-07 21:07:56 +0500

    update

---
+++ BloCYT33/Lecture14/code/null_check_handling/lib/Model/person_model.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'person_model.freezed.dart';
+part 'person_model.g.dart';
+
+@Freezed()
+class PersonModel with _$PersonModel {
+  const factory PersonModel({required String name, required int age}) =
+      _PersonModel;
+
+  factory PersonModel.fromJson(Map<String, dynamic> json) =>
+      _$PersonModelFromJson(json);
+}
