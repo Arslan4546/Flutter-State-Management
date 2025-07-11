872a2d41d20ceeee1e4417c9e7f3ab194498a284
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-11 16:43:11 +0500

    update

--- BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Bloc/Login_bloc/login_bloc.dart
+++ BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Bloc/Login_bloc/login_bloc.dart
@@ -4,9 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_event.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_states.dart';
 import 'package:splash_screen_practice/Repositories/Login_Repositories/login_repo.dart';
+import 'package:splash_screen_practice/Utils/Enums/api_enum.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginStates> {
-  LoginRepo _loginRepo;
+  final LoginRepo _loginRepo = LoginRepo();
   LoginBloc() : super(const LoginStates()) {
     on<EmailEvent>(_emailBlocFun);
     on<PasswordEvent>(_passwordBlocFun);
@@ -21,5 +22,38 @@ class LoginBloc extends Bloc<LoginEvent, LoginStates> {
     emit(state.copyWith(email: event.password));
   }
 
-  void _loginFun(SubmitButtonEvent event, Emitter<LoginStates> emit) {}
+  void _loginFun(SubmitButtonEvent event, Emitter<LoginStates> emit) {
+    Map<String, dynamic> data = {
+      "email": state.email,
+      "password": state.password,
+    };
+
+    _loginRepo
+        .loginAPI(data)
+        .then((value) {
+          if (state.errorMessage.isEmpty) {
+            emit(
+              state.copyWith(
+                apiStatus: APIStatus.success,
+                errorMessage: "Login Successfully",
+              ),
+            );
+          } else {
+            emit(
+              state.copyWith(
+                apiStatus: APIStatus.failed,
+                errorMessage: value.error.toString(),
+              ),
+            );
+          }
+        })
+        .onError((error, stackTrace) {
+          emit(
+            state.copyWith(
+              apiStatus: APIStatus.failed,
+              errorMessage: error.toString(),
+            ),
+          );
+        });
+  }
 }
--- BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Views/UI/Widgets/Login_Widgets/login_button_widget.dart
+++ BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Views/UI/Widgets/Login_Widgets/login_button_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_bloc.dart';
+import 'package:splash_screen_practice/Bloc/Login_bloc/login_event.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_states.dart';
+import 'package:splash_screen_practice/Utils/Enums/API_enum.dart';
 
 class LoginButtonWidget extends StatelessWidget {
   final formKey;
@@ -10,13 +12,31 @@ class LoginButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<LoginBloc, LoginStates>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state.apiStatus == APIStatus.loading) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text("Submitting...")));
+        } else if (state.apiStatus == APIStatus.success) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text("Login Successful")));
+        } else if (state.apiStatus == APIStatus.failed) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text(state.errorMessage.toString())),
+            );
+        }
+      },
       child: BlocBuilder<LoginBloc, LoginStates>(
         buildWhen: (previous, current) => false,
         builder: (context, state) {
           return ElevatedButton(
             onPressed: () {
-              if (formKey.currentState!.validate()) {}
+              if (formKey.currentState!.validate()) {
+                context.read<LoginBloc>().add(SubmitButtonEvent());
+              }
             },
             child: const Text('Login'),
           );
