31e21f8128e4024e228cb425a749ed8b377355d4
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-11 16:26:36 +0500

    update

--- BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Bloc/Login_bloc/login_bloc.dart
+++ BloCYT/Clean Architecture With BLoc/Lecture22/code/user_model_and_login_api/lib/Bloc/Login_bloc/login_bloc.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_event.dart';
 import 'package:splash_screen_practice/Bloc/Login_bloc/login_states.dart';
+import 'package:splash_screen_practice/Repositories/Login_Repositories/login_repo.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginStates> {
+  LoginRepo _loginRepo;
   LoginBloc() : super(const LoginStates()) {
     on<EmailEvent>(_emailBlocFun);
     on<PasswordEvent>(_passwordBlocFun);
+    on<SubmitButtonEvent>(_loginFun);
   }
 
   void _emailBlocFun(EmailEvent event, Emitter<LoginStates> emit) {
@@ -17,4 +20,6 @@ class LoginBloc extends Bloc<LoginEvent, LoginStates> {
   void _passwordBlocFun(PasswordEvent event, Emitter<LoginStates> emit) {
     emit(state.copyWith(email: event.password));
   }
+
+  void _loginFun(SubmitButtonEvent event, Emitter<LoginStates> emit) {}
 }
