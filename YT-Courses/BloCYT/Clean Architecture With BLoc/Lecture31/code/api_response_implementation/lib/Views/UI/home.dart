55199f225d403fd9e851562504b9324fee38e21a
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-12 20:09:59 +0500

    update

--- YT-Courses/BloCYT/Clean Architecture With BLoc/Lecture31/code/api_response_implementation/lib/Views/UI/home.dart
+++ YT-Courses/BloCYT/Clean Architecture With BLoc/Lecture31/code/api_response_implementation/lib/Views/UI/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart';
 import 'package:splash_screen_practice/Bloc/Movie_Bloc/movie_bloc.dart';
+import 'package:splash_screen_practice/Bloc/Movie_Bloc/movie_events.dart';
+import 'package:splash_screen_practice/Bloc/Movie_Bloc/movie_states.dart';
 import 'package:splash_screen_practice/Configs/Routes/route_names.dart';
+import 'package:splash_screen_practice/Data/Response/api_response.dart';
 import 'package:splash_screen_practice/Repositories/Movie_Repositories/movie_repository.dart';
 import 'package:splash_screen_practice/Services/Storage/app_local_storage.dart';
 import 'package:splash_screen_practice/main.dart';
@@ -41,7 +45,54 @@ class _HomeState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Center(child: Text("This is Home Screen")),
+      body: BlocProvider(
+        create: (_) => _movieBloc..add(MoviesFetchEvent()),
+        child: BlocBuilder<MovieBloc, MovieStates>(
+          buildWhen: (previous, current) =>
+              previous.moviesList != current.moviesList,
+          builder: (BuildContext context, state) {
+            switch (state.moviesList.apiStatus) {
+              case ApiResponse.loading:
+                return const Center(child: CircularProgressIndicator());
+              case ApiResponse.failed:
+                return Text(
+                  state.moviesList.errorMessage.toString(),
+                ); // Widget for displaying error state
+              case ApiResponse.completed:
+                if (state.moviesList.data == null) {
+                  // Widget for displaying no data found message
+                  return Text("No data Found");
+                }
+                final movieList = state.moviesList.data!;
+
+                return ListView.builder(
+                  itemCount: movieList.tvShow.length,
+                  itemBuilder: (context, index) {
+                    final tvShow = movieList.tvShow[index];
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(
+                          tvShow.imageThumbnailPath.toString(),
+                        ), // Widget for displaying network image
+                        title: Text(
+                          tvShow.name.toString(),
+                        ), // Title of the movie
+                        subtitle: Text(
+                          tvShow.network.toString(),
+                        ), // Network of the movie
+                        trailing: Text(
+                          tvShow.status.toString(),
+                        ), // Status of the movie
+                      ),
+                    );
+                  },
+                );
+              default:
+                return const SizedBox();
+            }
+          },
+        ),
+      ),
     );
   }
 }
