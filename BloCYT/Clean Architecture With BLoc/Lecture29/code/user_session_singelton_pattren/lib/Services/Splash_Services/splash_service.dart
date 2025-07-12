6e422b8691d049d4267801075840cb8c47551457
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-12 10:33:47 +0500

    manage user session completed

--- BloCYT/Clean Architecture With BLoc/Lecture29/code/user_session_singelton_pattren/lib/Services/Splash_Services/splash_service.dart
+++ BloCYT/Clean Architecture With BLoc/Lecture29/code/user_session_singelton_pattren/lib/Services/Splash_Services/splash_service.dart
@@ -2,16 +2,42 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:splash_screen_practice/Configs/Routes/route_names.dart';
+import 'package:splash_screen_practice/Services/Session_Manager/session_controller.dart';
 
 class SplashService {
-  void isLogin(BuildContext context) {
-    Timer(
-      Duration(seconds: 3),
-      () => Navigator.pushNamedAndRemoveUntil(
-        context,
-        RouteNames.loginScreen,
-        (route) => false,
-      ),
-    );
+  static void isLogin(BuildContext context) {
+    SessionController()
+        .getUserFromPreference()
+        .then((value) {
+          if (SessionController.isLogin!) {
+            Timer(
+              Duration(seconds: 3),
+              () => Navigator.pushNamedAndRemoveUntil(
+                context,
+                RouteNames.homeScreen,
+                (route) => false,
+              ),
+            );
+          } else {
+            Timer(
+              Duration(seconds: 3),
+              () => Navigator.pushNamedAndRemoveUntil(
+                context,
+                RouteNames.loginScreen,
+                (route) => false,
+              ),
+            );
+          }
+        })
+        .onError((error, stackTrace) {
+          Timer(
+            Duration(seconds: 3),
+            () => Navigator.pushNamedAndRemoveUntil(
+              context,
+              RouteNames.loginScreen,
+              (route) => false,
+            ),
+          );
+        });
   }
 }
--- BloCYT/Clean Architecture With BLoc/Lecture29/code/user_session_singelton_pattren/lib/Views/UI/splash_screen.dart
+++ BloCYT/Clean Architecture With BLoc/Lecture29/code/user_session_singelton_pattren/lib/Views/UI/splash_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen_practice/Services/Splash_Services/splash_service.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    SplashService.isLogin(context);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
