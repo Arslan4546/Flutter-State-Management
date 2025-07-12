a0c1576ef0d9ab70d47dab81b17ec2157057ba54
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-12 20:11:12 +0500

    update

--- YT-Courses/BloCYT/Clean Architecture With BLoc/Lecture31/code/api_response_implementation/lib/main.dart
+++ YT-Courses/BloCYT/Clean Architecture With BLoc/Lecture31/code/api_response_implementation/lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:splash_screen_practice/Configs/Routes/route_names.dart';
 import 'package:splash_screen_practice/Configs/Routes/routes.dart';
+import 'package:splash_screen_practice/Repositories/Login_Repositories/http_request.dart';
+import 'package:splash_screen_practice/Repositories/Login_Repositories/login_repo.dart';
 import 'package:splash_screen_practice/Repositories/Movie_Repositories/movie_http_request.dart';
 import 'package:splash_screen_practice/Repositories/Movie_Repositories/movie_repository.dart';
-import 'package:splash_screen_practice/Views/UI/login.dart';
 
 GetIt getIt = GetIt.instance;
 void main() {
@@ -30,5 +31,6 @@ class MyApp extends StatelessWidget {
 
 // phly base class do and then phir jo required hai mock ya http wali then wo do
 void serviceLocator() {
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepoHttp());
   getIt.registerLazySingleton<MovieRepo>(() => MovieHttpRepo());
 }
