309337eaed89e36b5b8e5714a034dc7842e5355e
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-12 14:17:32 +0500

    update

--- YT-Courses/GetX-YT/complete_getx_guide/lib/favoritePractice/homeScreen.dart
+++ YT-Courses/GetX-YT/complete_getx_guide/lib/favoritePractice/homeScreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getxdemo/favoritePractice/favoriteController.dart';
@@ -11,42 +10,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _TestingscreenState extends State<HomeScreen> {
-
-Favoritecontroller favoritecontroller = Get.put(Favoritecontroller());
+  Favoritecontroller favoritecontroller = Get.put(Favoritecontroller());
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: const Text("Favorite Practice",style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold
-        ),),
+        title: const Text(
+          "Favorite Practice",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
-      body:ListView.builder(
-          itemCount:favoritecontroller.fruitList.length,
-        itemBuilder: (context, index){
-            return Card(
-              child:  ListTile(
-                onTap: (){
-
-                  if(favoritecontroller.tempList.contains(favoritecontroller.fruitList[index].toString())){
-
-                 favoritecontroller.removeController(favoritecontroller.fruitList[index].toString());
-
-                  }else{
-                    favoritecontroller.addController(favoritecontroller.fruitList[index].toString());
+      body: ListView.builder(
+        itemCount: favoritecontroller.fruitList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                onTap: () {
+                  if (favoritecontroller.tempList.contains(
+                      favoritecontroller.fruitList[index].toString())) {
+                    favoritecontroller.removeController(
+                        favoritecontroller.fruitList[index].toString());
+                  } else {
+                    favoritecontroller.addController(
+                        favoritecontroller.fruitList[index].toString());
                   }
                 },
                 title: Text(favoritecontroller.fruitList[index].toString()),
-                trailing: Obx(()=>Icon(Icons.favorite,
-                  color: favoritecontroller.tempList.contains(favoritecontroller.fruitList[index].toString()) ? Colors.red : Colors.white,),)
-              ),
-            );
-
+                trailing: Obx(
+                  () => Icon(
+                    Icons.favorite,
+                    color: favoritecontroller.tempList.contains(
+                            favoritecontroller.fruitList[index].toString())
+                        ? Colors.red
+                        : Colors.white,
+                  ),
+                )),
+          );
         },
-
       ),
     );
   }
