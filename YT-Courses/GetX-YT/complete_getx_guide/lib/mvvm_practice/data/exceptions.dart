12eea0cfc7b9d074acff8d27dbe050192d0c6bd6
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-13 13:24:07 +0500

    updated

--- YT-Courses/GetX-YT/complete_getx_guide/lib/mvvm_practice/data/exceptions.dart
+++ YT-Courses/GetX-YT/complete_getx_guide/lib/mvvm_practice/data/exceptions.dart
@@ -2,39 +2,30 @@ class AppExceptions implements Exception {
   final _message;
   final _prefix;
 
-  AppExceptions([this._message,this._prefix]);
+  AppExceptions([this._message, this._prefix]);
 
-  String toString(){
+  String toString() {
     return "$_message$_prefix";
-
   }
-
-
 }
 
-class InternetExceptions extends AppExceptions{
-
-  InternetExceptions([String? message]): super (message, "");
-
-
-  }
+class InternetExceptions extends AppExceptions {
+  InternetExceptions([String? message]) : super(message, "");
+}
 
 class TimeOutExceptions extends AppExceptions {
-
-  TimeOutExceptions([String? message]) : super (message, "");
+  TimeOutExceptions([String? message]) : super(message, "");
 }
 
 class ServerExceptions extends AppExceptions {
-
-  ServerExceptions([String? message]) : super (message, "");
+  ServerExceptions([String? message]) : super(message, "");
 }
 
 class FetchingDataExceptions extends AppExceptions {
-
-  FetchingDataExceptions([String? message]) : super (message, "Error While Fetching Data");
+  FetchingDataExceptions([String? message])
+      : super(message, "Error While Fetching Data");
 }
 
 class InvalidURLException extends AppExceptions {
-
-  InvalidURLException([String? message]) : super (message, "Invalid URL");
+  InvalidURLException([String? message]) : super(message, "Invalid URL");
 }
