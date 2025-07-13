6681d1ef374f216a94eec39bfff1ae89a3c1a13e
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-13 13:28:52 +0500

    app exception code updated

--- YT-Courses/GetX-YT/complete_getx_guide/lib/mvvm_practice/data/exceptions.dart
+++ YT-Courses/GetX-YT/complete_getx_guide/lib/mvvm_practice/data/exceptions.dart
@@ -1,24 +1,25 @@
 class AppExceptions implements Exception {
-  final _message;
-  final _prefix;
+  final String? _message;
+  final String? _prefix;
 
   AppExceptions([this._message, this._prefix]);
 
+  @override
   String toString() {
     return "$_message$_prefix";
   }
 }
 
 class InternetExceptions extends AppExceptions {
-  InternetExceptions([String? message]) : super(message, "");
+  InternetExceptions([String? message]) : super(message, "Internet Exception");
 }
 
 class TimeOutExceptions extends AppExceptions {
-  TimeOutExceptions([String? message]) : super(message, "");
+  TimeOutExceptions([String? message]) : super(message, "Time OUt Exception");
 }
 
 class ServerExceptions extends AppExceptions {
-  ServerExceptions([String? message]) : super(message, "");
+  ServerExceptions([String? message]) : super(message, "Server Exception");
 }
 
 class FetchingDataExceptions extends AppExceptions {
