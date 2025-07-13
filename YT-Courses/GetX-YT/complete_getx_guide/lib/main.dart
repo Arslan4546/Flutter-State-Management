7d84d9f38fb1331e9e8307005449fa7f669489d3
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-13 10:42:31 +0500

    getx light and dart  mode start

--- YT-Courses/GetX-YT/complete_getx_guide/lib/main.dart
+++ YT-Courses/GetX-YT/complete_getx_guide/lib/main.dart
@@ -32,19 +32,19 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       //locale: const Locale("en", "US"),
-      locale: const Locale("en", "US"),
-      fallbackLocale: const Locale("en", "US"),
-      translations: Translate(),
-      home: const HomeScreen(),
+      // locale: const Locale("en", "US"),
+      // fallbackLocale: const Locale("en", "US"),
+      // translations: Translate(),
+      home: LightDarkTheme(),
       // getPages: [
       //   GetPage(name: "/S1", page:() => const Screen1()),
       //   GetPage(name: "/S2", page:() => Screen2()),
       // ],
 
-      getPages: AppRoutes.appRoutes(),
+      // getPages: AppRoutes.appRoutes(),
     );
   }
 }
